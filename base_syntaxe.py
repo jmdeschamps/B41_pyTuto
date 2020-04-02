ff80c07639dba5058c107bcc0fd6e673379809af
jmdeschamps <jmdeschamps@gmail.com>
2020-04-02 14:25:45 -0400

    Fichiers de notions et règles de syntaxes de base

---
+++ base_syntaxe.py
@@ -0,0 +1,32 @@
+## -*- Encoding: UTF-8 -*-  
+# la ligne ci-haut doit apparaître en entête (première ligne) de votre fichier pour que python comprenne les accents
+
+# COMMENTAIRES
+
+# le dièse (#) sert à commenter le reste de la ligne
+
+# QUELQUES RÈGLES
+
+#  la casse des caractères compte   g pas égal à G
+
+
+# TYPES DE BASE
+
+2 # un entier
+
+4.2 # un réel
+
+"une chaîne de caractères" # une chaîne de ...
+
+'une autre chaîne de carcatères'  # guillemets simples ou doubles MAIS pas mélangés
+
+""" Une
+    chaîne
+    multiligne
+"""
+
+# VARIABLES
+
+a="une valeur" 
+
+print("été")
