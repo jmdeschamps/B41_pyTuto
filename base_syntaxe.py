2893157157ae8838f32c7336a95d26eb770a64ab
jmdeschamps <jmdeschamps@gmail.com>
2020-04-04 16:28:55 -0400

    ajouter les transtypages

--- base_syntaxe.py
+++ base_syntaxe.py
@@ -1,16 +1,23 @@
 ## -*- Encoding: UTF-8 -*-  
 # la ligne ci-haut doit apparaître en entête (première ligne) de votre fichier pour que python comprenne les accents
 
-# COMMENTAIRES
+### COMMENTAIRES
 
 # le dièse (#) sert à commenter le reste de la ligne
 
-# QUELQUES RÈGLES
+### QUELQUES RÈGLES
 
 #  la casse des caractères compte   g pas égal à G
 
 
-# TYPES DE BASE
+##################################################################################
+# Les espaces blancs (espace, tabulation, nouvelle ligne) servent à formatter le code en bloc
+#        NE JOUER PAS AVEC LES INDENTATIONS ET ESPACES EN DÉBUT DE LIGNE...
+##################################################################################
+
+### TYPES DE BASE
+
+None # une valeur nulle, à utiliser au besoin, on en reparlera... 
 
 2 # un entier
 
@@ -24,9 +31,58 @@
     chaîne
     multiligne
 """
+'''
+    avec 
+    des
+    simples
+'''
 
-# VARIABLES
+[1,4,7,2] # une liste (crochet carré) !!!  les listes servent comme matrices, array, tableau de données
+
+[[12,43],[8,4],[22,55]] # une liste de liste, comme un tableau à 2 dimensions
+
+{}
+
+### VARIABLES
+# les variables n'ont pas à être déclarées mais doivent se faire assigner une valeur
+# chaque variable est typée, au moment de l'assignation, par son contenu
 
 a="une valeur" 
+b="" # une chaîne vide
+c=2
+d=4.2
+
 
 print("été")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
